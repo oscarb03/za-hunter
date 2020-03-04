ff7a377cf6d178dba51f2059c0966fc01dd572ab
Oscar <obankowski@gmail.com>
2020-03-04 10:56:20 -0600

    show segue after clicking i simbol

---
+++ za hunter/LocationDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationDetailsViewController.swift
+//  za hunter
+//
+//  Created by Oscar Bankowski on 3/4/20.
+//  Copyright © 2020 Oscar Bankowski. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: Codable {
+
+}
